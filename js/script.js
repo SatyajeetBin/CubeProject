d826336ac11881a8cd0a95217bdb57b9d26b2645
SatyajitBin <satyajit.binstellar@gmail.com>
2025-12-26 18:21:39 +0530

    chnage css

--- js/script.js
+++ js/script.js
@@ -84,3 +84,17 @@ subRadios.forEach(r => {
     }
   });
 });
+
+
+// Collection Section
+
+document.querySelectorAll('.faq-item').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.faq-item').forEach(i => {
+      i.classList.remove('active');
+      i.querySelector('.icon').textContent = '+';
+    });
+    item.classList.add('active');
+    item.querySelector('.icon').textContent = '−';
+  });
+});
